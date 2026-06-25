eb453f66f68bef2a503799958e18b5545d92712c
kandarpa02 <kandarpaexe@gmail.com>
2026-06-25 13:24:49 +0530

    resnet50

--- resnet_training/__init__.py
+++ resnet_training/__init__.py
@@ -1,5 +1,6 @@
 from .data import cifar10
 from .resnet18 import ResNet18, init
+from .resnet50 import ResNet50
 from .train import trainer
 from .checkpoint import Checkpoint
 from .base import param_count
